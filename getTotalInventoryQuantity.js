3c4e8cba059c16f4101f189f3d3caf4d87d44887
Ryan Chang <rchang@videoamp.com>
2019-10-18 13:57:34 -0700

    adds blacklist

--- getTotalInventoryQuantity.js
+++ getTotalInventoryQuantity.js
@@ -4,6 +4,20 @@ import {
 } from './api'
 import { getInventories } from './helpers'
 
+// Change the array below to NOT count certain barcodes.
+const INVENTORY_BARCODE_BLACKLIST = [
+  // Core Embroidered Sock
+  '18CCSO1BL',
+  '18CCSO1WH',
+  '18CCSO1GR',
+  // Core Rope Sock
+  '18CCS2B',
+  '18CCS2G',
+  '18CCS2W',
+  // BBC Sock
+  '18BBCAOS'
+]
+
 /**
  *  Returns the total number of units in El Monte
  */
@@ -20,6 +34,10 @@ async function getTotalInventoryQuantity () {
   const inventories = await getInventories(productIds)
 
   const total = inventories.reduce((memo, inventory) => {
+    if (INVENTORY_BARCODE_BLACKLIST.includes(inventory.product.barcode)) {
+      return memo
+    }
+
     return memo + inventory.quantity
   }, 0)
 
@@ -28,6 +46,8 @@ async function getTotalInventoryQuantity () {
 
 export const handler = async (event, context) => {
   const total = await getTotalInventoryQuantity()
+
+  console.log(`Total units at EasyPost: ${total}`)
 
   return {
     statusCode: 200,
